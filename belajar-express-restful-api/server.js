f7ceb8cb998c972fb0887a7d442ad1696cac66f7
Admantix <williamwijaya499@gmail.com>
2025-05-11 20:13:53 +0700

    fix: set PORT to a fixed value of 3000 in server configuration

--- belajar-express-restful-api/server.js
+++ belajar-express-restful-api/server.js
@@ -1,6 +1,6 @@
 // ini tempat buat jalanin server-nya
 const app = require("./app");
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
