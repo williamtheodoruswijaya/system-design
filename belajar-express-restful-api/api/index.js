faa8301a4fe7220660b502af12827043c94886ef
Admantix <williamwijaya499@gmail.com>
2025-05-11 20:16:42 +0700

    style: format vercel.json for improved readability

--- belajar-express-restful-api/api/index.js
+++ belajar-express-restful-api/api/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const userRoutes = require("../routes/userRoutes");
-
 const app = express();
+
 app.use(express.json());
 app.use("/users", userRoutes);
 
