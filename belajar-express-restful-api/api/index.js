4e49273e5f1159ca679c3ae632b95697aaa90818
Admantix <williamwijaya499@gmail.com>
2025-05-11 20:17:50 +0700

    fix: add error handling and debug logging to the Express server

--- belajar-express-restful-api/api/index.js
+++ belajar-express-restful-api/api/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const userRoutes = require("../routes/userRoutes");
-const app = express();
 
+const app = express();
 app.use(express.json());
+
+// Debug log
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+});
+
 app.use("/users", userRoutes);
+
+// Catch unhandled errors
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ status: "error", message: "Internal Server Error" });
+});
 
 module.exports.handler = serverless(app);
