5e6e0dfe60e174ece74e699a5ab3346794f53617
Admantix <williamwijaya499@gmail.com>
2025-05-11 20:27:34 +0700

    refactor: simplify SSL configuration for database connection

--- belajar-express-restful-api/config/db.js
+++ belajar-express-restful-api/config/db.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 module.exports = pool;
