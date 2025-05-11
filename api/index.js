4cdb5c99ba90f0cb38d43cd0e35bdb87a6979f3e
Admantix <williamwijaya499@gmail.com>
2025-05-11 19:33:23 +0700

    Remove unnecessary files and routes for user management and server setup

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const userRoutes = require("../routes/userRoutes");
-
-const app = express();
-app.use(express.json());
-app.use("/users", userRoutes);
-
-module.exports.handler = serverless(app);
